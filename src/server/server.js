e8fa711f05b2c63dccc68792cac9a539815bc867
Marina Saitgareeva <4006761ms@gmail.com>
2022-09-18 13:01:34 -0400

    add fetch from Weatherbit, GeoNames and Pixabay

--- src/server/server.js
+++ src/server/server.js
@@ -23,12 +23,7 @@ app.use(bodyParser.json()); // to use json
 const cors = require('cors');
 app.use(cors());
 
-// Require module node-fetch to use the fetch() function in NodeJS
-// const fetch = require('node-fetch');
-const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
-
-  // Initialize the main project folder
+// Initialize the main project folder
 app.use(express.static('dist'));
 
 console.log(__dirname);
@@ -38,11 +33,80 @@ const port = 8080;
 
 // Designates what port the app will listen to for incoming requests
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+  console.log(`*** Travel app is listening on port ${port}! ***`)
 });
-
 
 // GET route
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('dist/index.html'))
+  res.sendFile(path.resolve('dist/index.html'))
 })
+
+// Import functions
+import { fetchGeonamesApi } from './geoNamesAPI';
+import { fetchWeatherbitApi } from './weatherbitAPI';
+import { fetchPixabayApi } from './pixabayAPI';
+
+// Set variable for trip data from different API
+const trip = {
+  departure: 'from',
+  destination: {
+    city: 'to',
+    country: '',
+    country_code: '',
+    latitude: '',
+    longitude: '',
+    // population: ''
+  },
+  id: '',
+  date: '',
+  countdown: '',
+  weather: {
+      temperature: '',
+      icon: '',
+      description: ''
+  },
+  // flight: {
+  //     price: '',
+  //     carrier: '',
+  //     direct: ''
+  // },
+  image: '',
+  // covid: {
+  //   growth: '',
+  //   level: ''
+  // }
+};
+
+// POST route - trip data
+app.post('/tripInfo', async (req, res) => {
+  // Set destination city and departing date
+  trip.departure = req.body.departure;
+  trip.date = req.body.date;
+  // get countdown number
+  trip.countdown = countdown(req.body.date).toString();
+
+  // Fetch destination data by GeoNamesAPI
+  let destinationData = await fetchGeonamesApi(req.body.destination, process.env.GEONAMES_USERNAME);
+  trip.destination.city = destinationData.city;
+  trip.destination.country_code = destinationData.country_code;
+  trip.destination.latitude = destinationData.latitude;
+  trip.destination.longitude = destinationData.longitude;
+
+  // Fetch weather data from wWatherbit API
+  let weatherData = await fetchWeatherbitApi(
+    trip.destination.latitude,
+    trip.destination.longitude,
+    trip.date,
+    process.env.WEATHERBIT_API_KEY
+  );
+  // console.log(weatherData);
+  trip.weather.temperature = weatherData.temperature;
+  trip.weather.icon = weatherData.weather_icon;
+  trip.weather.description = weatherData.weather_description;
+
+  // Fetch image url by Pixabay API
+  trip.image = await fetchPixabayApi(req.body.destination, '', process.env.PIXABAY_API_KEY);
+
+  res.send(trip);
+  console.info('** This request has been processed:\n', req.body, ' **');
+});
