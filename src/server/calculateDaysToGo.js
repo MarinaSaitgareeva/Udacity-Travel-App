2c81fe7d85df96ae9baae5522c1af3e339a9fe30
Marina Saitgareeva <4006761ms@gmail.com>
2022-09-24 19:07:59 -0400

    add module.exports, fix bug

--- src/server/calculateDaysToGo.js
+++ src/server/calculateDaysToGo.js
@@ -1,10 +1,11 @@
 // This function determines a number of days between travel start date and current date
 const calculateDaysToGo = (tripStartDate) => {
   let currentDate = new Date();
-  let tripStartDate = new Date(tripStartDate);
+  tripStartDate = new Date(tripStartDate);
   let differenceInDays = tripStartDate - currentDate;
   let daysToGo = new Date(differenceInDays) / (24 * 3600 * 1000);
   return Number(Math.round(daysToGo));
 };
 
-export { calculateDaysToGo };
+// Export js file
+module.exports = calculateDaysToGo;
