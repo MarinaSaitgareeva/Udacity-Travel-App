6629b75efd769819eb5ab14834d942de65a55a76
Marina Saitgareeva <4006761ms@gmail.com>
2022-09-24 19:10:21 -0400

    change url

--- src/server/pixabayAPI.js
+++ src/server/pixabayAPI.js
@@ -4,24 +4,24 @@ const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 // Fetch Pixabay API with img
-const fetchPixabayApi = async (city = '', country = '', paxabayApiKey) => {
-  let url = 'https://pixabay.com/api/?';
-  url = `${url}key=${paxabayApiKey}&q=${city}+${country}&category=travel`;
+const fetchPixabayApi = async (city = '', country = '', pixabayApiKey) => {
+  let url = `https://pixabay.com/api/?key=${pixabayApiKey}&q=${city}+${country}&category=travel`;
 
   let response = await fetch(url);
-  console.log('Pixabay API: ', response.status, response.statusText, response.ok);
+  console.log(`Pixabay API: ${response.status} ${response.statusText} ${response.ok}`);
 
   if (response.ok) {
     let data = await response.json();
     if (data.hits.length > 0) {
-        // console.log(data.hits);
-        return data.hits[0].webformatURL;
+      // console.log(data.hits);
+      return data.hits[0].webformatURL;
     }
   } else {
-    console.log(`ERROR: code ${response.status} ${response.statusText}.`);
+    // Appropriately handle the error
+    console.log(`Error: ${response.status} ${response.statusText}.`);
   }
-  return 'http://placecorgi.com/250';
-  // return 'no data';S
+  return 'no data';
 };
 
-  export { fetchPixabayApi };
+// Export js file
+module.exports = fetchPixabayApi;
