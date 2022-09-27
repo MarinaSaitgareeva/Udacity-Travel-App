fdd2830ef99ca1862e57e693941b874c262496bd
Marina Saitgareeva <4006761ms@gmail.com>
2022-09-26 22:11:21 -0400

    add random number for different city's img

--- src/server/pixabayAPI.js
+++ src/server/pixabayAPI.js
@@ -1,9 +1,7 @@
 // Require module node-fetch to use the fetch() function in NodeJS
-// const fetch = require('node-fetch');
-const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-// Fetch Pixabay API with img
+// Fetch Pixabay API with img based on city and country name
 const fetchPixabayApi = async (city = '', country = '', pixabayApiKey) => {
   let url = `https://pixabay.com/api/?key=${pixabayApiKey}&q=${city}+${country}&category=travel&image_type=photo`;
 
@@ -13,14 +11,13 @@ const fetchPixabayApi = async (city = '', country = '', pixabayApiKey) => {
   if (response.ok) {
     let data = await response.json();
     if (data.hits.length > 0) {
-      // console.log(data.hits);
-      return data.hits[0].webformatURL;
+      return data.hits[Math.floor(Math.random() * 10)].webformatURL; // Random number => different city's img
     }
   } else {
-    // Appropriately handle the error
-    console.log(`Error: ${response.status} ${response.statusText}.`);
-  }
-  return 'no data';
+      // Appropriately handle the error
+      console.log(`Error: ${response.status} ${response.statusText}!!!`);
+      return 'no data';
+    };
 };
 
 // Export js file
