87ec816cbf75dcbcdc68d6f1252f81f63883cf82
Marina Saitgareeva <4006761ms@gmail.com>
2022-09-27 00:36:55 -0400

    add displayNewSearch

--- src/client/js/displayNewSearch.js
+++ src/client/js/displayNewSearch.js
@@ -1,32 +1,54 @@
 // Function for search result
 export const displayNewSearch = (trip) => {
 
-  // New departure
-  // document.querySelector('#departure-name').textContent = `My trip from: ${trip.departure.city}, ${trip.departure.country}`;
-
-  // New destination
-  document.querySelector('#destination-name').textContent = `My trip from ${trip.departure.city}, ${trip.departure.country} to ${trip.destination.city}, ${trip.destination.country}`;
-  // document.querySelector('#destination-name').textContent = `My trip to: ${trip.destination.city}, ${trip.destination.country}`;
+  // New departure and destination
+  document.querySelector('#destination-and-departure').innerHTML = `<strong>Route: </strong>from <strong><a class="map" href="${trip.departure.map}" target="_blank">${trip.departure.city} (${trip.departure.country_code}) <img class="flag" src="${trip.departure.flag}" alt="flag"></a></strong> to <strong><a class="map" href="${trip.destination.map}" target="_blank">${trip.destination.city} (${trip.destination.country_code}) <img class="flag" src="${trip.destination.flag}" alt="flag"></a></strong>`;
   
-  // New start date
-  document.querySelector('#start-date-actual').textContent = `Dates: start ${trip.startDate}, end ${trip.endDate}`;
+  // New start and end date (trip's length) - daysToGo
+  document.querySelector('#start-and-end-date').innerHTML = `<strong>Dates: </strong>from <strong> ${trip.startDate} </strong> till <strong> ${trip.endDate} </strong> (${trip.daysLength} days) - <em style="color: #faaa54;">${daysToGoText(trip.daysToGo)}</em>`;
 
-  // // New end date
-  // document.querySelector('#end-date-actual').textContent = `End date: ${trip.endDate}`;
+    // Function to add text in daysToGo
+    function daysToGoText() {
+      if (trip.daysToGo > 0) {
+        return `${trip.daysToGo} days left. Be Ready!`
+      }
+      else if (trip.daysToGo == 0) {
+        return `Today! Are You Ready?`
+      }
+      else {
+        return `Expired, You missed your Trip!`
+      }
+    };
 
-  // New daysToGo
-  document.querySelector('#day-to-go').textContent = `${trip.daysToGo} days left`;
+  // New destination details
+  document.querySelector('#destination-details').innerHTML = `
+    <p>
+      <strong>Population: </strong>${trip.destination.city_population};&nbsp; &nbsp; 
+      <strong>Language: </strong>${trip.destination.language};&nbsp; &nbsp; 
+      <strong>Currency: </strong>${trip.destination.currency};&nbsp; &nbsp; 
+      <strong>Capital city: </strong>${trip.destination.capital}
+    </p>`;
 
-  // New trip's length
-  document.querySelector('#day-length').textContent = `${trip.daysLength} days`;
+    document.querySelector('#current-time').innerHTML = `<strong>Current time: </strong>${trip.time.current_time} (${trip.time.abbreviation}: ${trip.time.gmtOffset})`;
 
   // New weather
-  document.querySelector('#weather-info').textContent = `\n${trip.weather.temperature} C, ${trip.weather.description}`;
-  document.querySelector('#weather-icon').src = trip.weather.icon;
+  document.querySelector('#weather').innerHTML = `
+    <p>The <strong>weather</strong> probably will be (based on historical data):</p>
+    <p>${trip.weather.historical_temperature} (feel likes = ${trip.weather.historical_feels_like_temperature}),</p>
+    <p>uv = ${trip.weather.historical_uv},</p>
+    <p>wind = ${trip.weather.historical_wind_speed} (${trip.weather.historical_wind_direction})</p>
+    <p id="weather_historical_description">${trip.weather.historical_description}</p>
+    <img id="weather-icon" src="${trip.weather.historical_icon}" alt="weather icon">`;
+
+  // document.querySelector('#weather-icon').src = trip.weather.historical_icon;
 
   // New image
   document.querySelector('#destination-photo').src = trip.image;
 
   // Show search result
   document.querySelector('#trip-result').classList.remove('hide');
+
+  // Hide search form after loading of result
+  document.querySelector('#trip-search').style.display = 'none';
+  document.querySelector('#trip-result').style.marginTop = '13.3rem';
 };
