8dd618be75766c3836c6b2fd498edb49411017cb
Marina Saitgareeva <4006761ms@gmail.com>
2022-09-27 00:36:03 -0400

    add event listener to save and remove trip btns

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,8 @@
 // Import js files
 import { setMinAttributeToDate } from './js/setMinAttributeToDate';
-// import { checkUrl } from './js/urlChecker'
 import { handleSubmit } from './js/handleSubmit';
+
+
 
 // Import style sass files
 import './styles/index.scss'
@@ -12,8 +13,11 @@ document.addEventListener('DOMContentLoaded', setMinAttributeToDate);
 // Add Event Listener to search button
 document.querySelector('#search-form-btn').addEventListener('click', handleSubmit);
 
+// Add Event Listener to save and delete buttons
+document.querySelector('#search-form-save-btn').addEventListener('click', performSave);
+document.querySelector('#search-form-remove-btn').addEventListener('click', performRemove);
+
 // Export js files
 export {
-  // checkUrl,
   handleSubmit
 }
