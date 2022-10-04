5fe51e70db98d2d753f5d45e1b3b5ea5a50c49e8
Marina Saitgareeva <4006761ms@gmail.com>
2022-10-03 23:45:24 -0400

    add comments

--- src/server/calculateDaysLength.js
+++ src/server/calculateDaysLength.js
@@ -1,8 +1,11 @@
-// This function determines a number of days between travel start date and end date
+// Function to determine a number of days between travel start date and end date
 const calculateDaysLength = (tripStartDate, tripEndDate) => {
+  // Set variables for trip start and end date
   tripStartDate = new Date(tripStartDate);
   tripEndDate = new Date(tripEndDate);
+  // Calculate difference between start date and current date
   let differenceInDays = tripEndDate - tripStartDate;
+  // Convert difference from milliseconds to days (24 - hours to days, 60*60=3600 - seconds to minutes then hours, 1000 - milliseconds to seconds)
   let daysLength = new Date(differenceInDays) / (24 * 3600 * 1000);
   return Number(Math.round(daysLength) + 1); // +1 including trip start date
 };
--- src/server/calculateDaysToGo.js
+++ src/server/calculateDaysToGo.js
@@ -1,8 +1,12 @@
-// This function determines a number of days between travel start date and current date
+// Function to determine a number of days between trip start date and current date
 const calculateDaysToGo = (tripStartDate) => {
+  // Set variable for current date
   let currentDate = new Date();
+  // Set variable for trip start date
   tripStartDate = new Date(tripStartDate);
+  // Calculate difference between start date and current date
   let differenceInDays = tripStartDate - currentDate;
+  // Convert difference from milliseconds to days (24 - hours to days, 60*60=3600 - seconds to minutes then hours, 1000 - milliseconds to seconds)
   let daysToGo = new Date(differenceInDays) / (24 * 3600 * 1000);
   return Number(Math.round(daysToGo) + 1); // +1 including current date
 };
