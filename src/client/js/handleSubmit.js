fc1b6762a005246384cb2962eab53edb58c3ea2f
Marina Saitgareeva <4006761ms@gmail.com>
2022-10-03 23:45:11 -0400

    add comments

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -2,31 +2,31 @@
 import { postTripInfo } from './postTripInfo';
 import { displayNewSearch } from './displayNewSearch';
 
-// Function that handles new search submit
+// Function to handle a new search submit
 async function handleSubmit(event) {
   event.preventDefault();
 
-  // Get input from the form filed by user
+  // Set variables for inputs (departure, destination, startDate, endDate) from the form filed by user
   let departure = document.querySelector('#departure').value;
   let destination = document.querySelector('#destination').value;
   let startDate = document.querySelector('#start-date').value;
   let endDate = document.querySelector('#end-date').value;
 
-  // Store form data in object for sending to server
-  let formData = {
+  // Store form inputs in object for sending to server
+  let formInputs = {
     departure: departure,
     destination: destination,
     startDate: startDate,
     endDate: endDate,
   };
-  console.log(departure, destination, startDate, endDate);
+  // console.log(departure, destination, startDate, endDate);
 
-  // Post the search results back to the Express server
-  trip = await postTripInfo(formData).then((res) => {
-    console.log(res);
-    // Then render the returned result in the UI
-    displayNewSearch(res);
-    return res;
+  // Post the search result back to the Express server
+  trip = await postTripInfo(formInputs).then((response) => {
+    // console.log(response);
+    // Then render the returned result in the UI (add search result to "trip-result" div)
+    displayNewSearch(response);
+    return response;
   });
 };
 
