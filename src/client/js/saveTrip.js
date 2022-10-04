64c07a685f50e11504efd3fdb4caf3892c1676fe
Marina Saitgareeva <4006761ms@gmail.com>
2022-10-03 23:44:49 -0400

    save trip to Local Storage

--- src/client/js/saveTrip.js
+++ src/client/js/saveTrip.js
@@ -1,55 +1,65 @@
 // Import js file
 import { renderSavedTripTemplate } from './renderSaveTripTemplate';
 
-// Function to save search result
-export const saveTrip = async () => {
-  let savedTrips = await getSavedTrips();
+// Function to save search result to "saved-trips" div
+const saveTrip = async () => {
+  // Set variable for search result, get data from the Express Server
   const searchResult = await getSearchResult();
+  // Set variable for ID
+  let id = searchResult.daysToGo + '-' + searchResult.destination.city.replace(/ /g,'') + '-' + searchResult.departure.city.replace(/ /g,'');
+  // Replace spaces " " in name of city and country with underscore "_"
+  // city = city.replace(/ /g,'');
 
-  postSaveTripInfo('/saveTripInfo', searchResult).then(async (savedTrip) => {
-      // Put the object into local storage
-      savedTrips = await getSavedTrips();
-      localStorage.setItem('savedTrips', JSON.stringify(savedTrips));
+  // Set ID for saved trips
+  searchResult.id = id;
 
-      // Create div element for storing saved trip info
-      const saveTripBox = document.createElement('div');
-      saveTripBox.classList.add('saved-trip-container');
-      saveTripBox.innerHTML = renderSavedTripTemplate(
-          savedTrip,
-          savedTrip.id
-      );
+  // Log with saved trip data
+  console.log('Save trip:', searchResult);
 
-      document.querySelector('#saved-trips').prepend(saveTripBox);
+  // Set variable to store saved trips in an array
+  let savedTripArray = [];
 
-      // Show saved trips section
-      document.querySelector('#saved-trips').classList.remove('hide');
+  // A conditional statement that checks if Local Storage already exists (otherwise after closing the browser or reloading the page, all the existing information in Local Storage is gone, and nothing remains on the front end)
+  if (localStorage.getItem('trips')) {
+    savedTripArray = JSON.parse(localStorage.getItem('trips'))
+  } else {
+    savedTripArray = []
+  };
 
-      // Hide search result
-      document.querySelector('#trip-result').classList.add('hide');
-  });
+  // Create a Local Storage key called "trips", using JSON.stringify() to convert a data array to a string
+  localStorage.setItem('trips', JSON.stringify(savedTripArray));
+
+  // Push new saved trip into the array
+  savedTripArray.push(searchResult);
+
+  // Set the Local Storage to the new, updated value
+  localStorage.setItem('trips', JSON.stringify(savedTripArray));
+
+  // Function to sort saved trips by their start date, destination city and departure city
+  const sortSavedTrips = (savedTrip1, savedTrip2) => (new Date(savedTrip1.startDate) - new Date(savedTrip2.startDate) || savedTrip1.destination.city.localeCompare(savedTrip2.destination.city) || savedTrip1.departure.city.localeCompare(savedTrip2.departure.city));
+
+  // Update savedTripArray with sorted saved trips
+  savedTripArray = savedTripArray.sort(sortSavedTrips);
+
+  // Log with updated saved trips from Local Storage
+  console.log('Updated saved trips:', savedTripArray);
+
+  // Clear "saved-trips" div (delete rendered saved trips from Local Storage)
+  document.querySelector('#saved-trips').innerHTML = '';
+
+  // Create "saved-trip-container" div with saved trip's data for each saved trip in Local Storage
+  savedTripArray.forEach(renderSavedTripTemplate);
+
+  // Hide search result "trip-result" div
+  document.querySelector('#trip-result').classList.add('hide');
 };
 
-// GET route for search result
+// GET route for search result, fetch data from the Express Server
 const getSearchResult = async () => {
-  const response = await fetch('/getTripInfo');
-  const searchResult = await response.json();
-  return searchResult;
+  const response = await fetch('/tripInfo');
+  // Extract a JSON object from the response
+  return await response.json()
 };
 
-// GET route for saved trips
-const getSavedTrips = async () => {
-  const response = await fetch('/getSavedTripInfo');
-  const savedTrips = await response.json();
-  return savedTrips;
-};
-
-const postSaveTripInfo = async (url = '', data = {}) => {
-  const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-  });
-  return response.json();
-};
+// Export js file
+export { saveTrip };
