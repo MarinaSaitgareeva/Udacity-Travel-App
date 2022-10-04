fc87c0389039c3c71aa80ff42bc4f54a48a12518
Marina Saitgareeva <4006761ms@gmail.com>
2022-10-04 11:24:35 -0400

    add calculateDaysToGo to client side

--- src/client/js/calculateDaysToGo.js
+++ src/client/js/calculateDaysToGo.js
@@ -1,5 +1,6 @@
-// Function to determine a number of days between trip start date and current date
-const calculateDaysToGo = (tripStartDate) => {
+// Function to calculate a number of days between trip start date and current date
+const calculateDaysToGo = (trip) => {
+  let tripStartDate = trip.startDate;
   // Set variable for current date
   let currentDate = new Date();
   // Set variable for trip start date
@@ -8,8 +9,8 @@ const calculateDaysToGo = (tripStartDate) => {
   let differenceInDays = tripStartDate - currentDate;
   // Convert difference from milliseconds to days (24 - hours to days, 60*60=3600 - seconds to minutes then hours, 1000 - milliseconds to seconds)
   let daysToGo = new Date(differenceInDays) / (24 * 3600 * 1000);
-  return Number(Math.round(daysToGo) + 1); // +1 including current date
+  return trip.daysToGo = (Number(Math.round(daysToGo) + 1)).toString() // +1 including current date
 };
 
 // Export js file
-module.exports = calculateDaysToGo;
+export { calculateDaysToGo };
