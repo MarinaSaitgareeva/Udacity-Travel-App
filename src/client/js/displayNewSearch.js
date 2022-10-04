374cde2d8e52c744dc6e8686f0c7c1cc94ce90d0
Marina Saitgareeva <4006761ms@gmail.com>
2022-10-03 23:47:16 -0400

    comment changes

--- src/client/js/displayNewSearch.js
+++ src/client/js/displayNewSearch.js
@@ -1,20 +1,20 @@
 // Import js file
 import { saveTrip } from './saveTrip';
 
-// Function to render search result
-export const displayNewSearch = (trip) => {
-  // New image
+// Function to render search result in "trip-result" div
+const displayNewSearch = (trip) => {
+  // Add image
   document.querySelector('#destination-photo').src = trip.image;
-  // New image caption
+  // Add image caption
   document.querySelector('#destination-photo-caption').textContent = trip.image_figcaption;
 
-  // New departure and destination
+  // Add departure and destination: city, country code, flag and map url
   document.querySelector('#destination-and-departure').innerHTML = `<strong>Route: </strong>from <strong><a class="map" href="${trip.departure.map}" target="_blank">${trip.departure.city} (${trip.departure.country_code}) <img class="flag" src="${trip.departure.flag}" alt="flag"></a></strong> to <strong><a class="map" href="${trip.destination.map}" target="_blank">${trip.destination.city} (${trip.destination.country_code}) <img class="flag" src="${trip.destination.flag}" alt="flag"></a></strong>`;
   
-  // New start and end date (trip's length) - daysToGo
+  // Add start and end dates (trip's length) - number of days between trip start date and current date
   document.querySelector('#start-and-end-date').innerHTML = `<strong>Dates: </strong>from <strong> ${formatDate(trip.startDate)} </strong> till <strong> ${formatDate(trip.endDate)} </strong> (${trip.daysLength} days) - <em style="color: #faaa54;">${daysToGoText(trip.daysToGo)}</em>`;
 
-    // Function to change format of date (start and end dates)
+    // Function to change format of date
     function formatDate (date) {
       let d = new Date(date);
       let months = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
@@ -40,7 +40,7 @@ export const displayNewSearch = (trip) => {
       }
     };
 
-  // New destination details
+  // Add destination details
   document.querySelector('#destination-details').innerHTML = `
     <p>
       <strong>Population: </strong>${trip.destination.city_population};&nbsp; &nbsp; 
@@ -50,10 +50,10 @@ export const displayNewSearch = (trip) => {
     </p>
   `;
 
-  // New current time (destination)
+  // Add current time (destination)
   document.querySelector('#current-time').innerHTML = `<strong>Current time: </strong>&nbsp;${trip.time.current_time} (${trip.time.abbreviation}: ${trip.time.gmtOffset})`;
 
-  // New weather
+  // Add weather (historic and current weather in destination)
   document.querySelector('#weather').innerHTML = `
     <p><strong>Historic weather </strong><i id="weather-assumption">(previous year) </i> : </p>
     <p>${trip.weather.historical_temperature} (feel likes = ${trip.weather.historical_feels_like_temperature}),</p>
@@ -64,29 +64,22 @@ export const displayNewSearch = (trip) => {
     </p>
    
     <p><strong>Current weather: </strong></p>
-    <p id="weather-current">${trip.weather.current_temperature}, uv = ${trip.weather.current_uv}, rh = ${trip.weather.current_humidity}, wind = ${trip.weather.current_wind_speed} (${trip.weather.current_wind_direction}), ${trip.weather.current_description} <img id="weather-icon" src="${trip.weather.current_icon}" alt="weather icon"></p>
+    <p id="weather-current">${trip.weather.current_temperature}, uv = ${trip.weather.current_uv}, rh = ${trip.weather.current_humidity}, wind = ${trip.weather.current_wind_speed} (${trip.weather.current_wind_direction}),</p>
+    <p id="weather-current-description"> ${trip.weather.current_description} <img id="weather-icon" src="${trip.weather.current_icon}" alt="weather icon"></p>
   `;
 
-  // Show search result
+  // Show search result in "trip-result" div
   document.querySelector('#trip-result').classList.remove('hide');
 
-  // Smoothly scrolls to Div with results
+  // Smoothly scrolls to "trip-result" div with search result
   document.querySelector('#trip-result').scrollIntoView(false, {
     behavior: 'smooth',
     block: 'end'
   });
 
-  // Add Event Listener to save and delete buttons
+  // Add Event Listener (click) to "search-form-save-btn" button (save trip in "saved-trips" div)
   document.querySelector('#search-form-save-btn').addEventListener('click', saveTrip);
-  // document.querySelector('#search-form-remove-btn').addEventListener('click', removeTrip());
-
-  // // Add function for saving trip
-  // document.querySelector('#search-form-save-btn').onclick = 'return saveTrip()';
-
-  // // Add function for removing trip
-  // document.querySelector('#search-form-remove-btn').onclick = 'return removeTrip()';
-  
-  // Hide search form after loading of result
-  // document.querySelector('#trip-search').style.display = 'none';
-  // document.querySelector('#trip-result').style.marginTop = '13.3rem';
 };
+
+// Export js file
+export { displayNewSearch };
