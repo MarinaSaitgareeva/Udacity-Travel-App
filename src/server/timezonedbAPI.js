6bccf1e1c87257859c6d38fafcbec6cfb447f815
Marina Saitgareeva <4006761ms@gmail.com>
2022-10-10 09:02:14 -0400

    style changes

--- src/server/timezonedbAPI.js
+++ src/server/timezonedbAPI.js
@@ -3,8 +3,9 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 
 // Fetch Timezonedb API with city time based on latitude, longitude
 const fetchTimezonedbApi = async (latitude, longitude, timezonedbApiKey) => {
+  // Set variable to store url to fetch API data
   let url = `http://api.timezonedb.com/v2.1/get-time-zone?&format=json&key=${timezonedbApiKey}&by=position&lat=${latitude}&lng=${longitude}`;
-
+  // Fetch API data
   let response = await fetch(url);
   console.log('Timezonedb API:', response.status, response.statusText, response.ok);
 
@@ -12,30 +13,17 @@ const fetchTimezonedbApi = async (latitude, longitude, timezonedbApiKey) => {
     let data = await response.json();
   
     return {
-      zone_name: data.zoneName,
       abbreviation: data.abbreviation, // Abbreviation of the time zone
       gmtOffset: (data.gmtOffset / 60 / 60).toString(), // The time offset in hours (seconds / 60 / 60) based on UTC time
-      current_time: data.formatted.toString().slice(11,16) // Formatted timestamp in Y-m-d h:i:s format. E.g.: 2022-09-26 16:23:22
     };
   } else {
     // Appropriately handle the error
       console.log(`Error: code ${response.status} ${response.statusText}!!!`);
       return {
-        zone_name: 'no data',
         abbreviation: 'no data',
-        gmtOffset: 'no data',
-        current_time: 'no data'
+        gmtOffset: 'no data'
       };
     }
-};
-
-//
-function changeGmtOffSet (gmtOffset) {
-  if (gmtOffset > 0) {
-    return `+${gmtOffset}`;
-  } else {
-    return `${gmtOffset}`;
-  };
 };
 
 // Export js file
--- src/server/weatherbitCurrentAPI.js
+++ src/server/weatherbitCurrentAPI.js
@@ -4,35 +4,38 @@ const fetch = (...args) =>
 
 // Fetch Weatherbit API with weather current data based on city, country, start date, end date (16 Day Weather Forecast API)
 const fetchWeatherbitCurrentApi = async (latitude, longitude, weatherbitApiKey) => {
+  // Set variable to store url to fetch API data
   let currentUrl = `https://api.weatherbit.io/v2.0/current?key=${weatherbitApiKey}&lat=${latitude}&lon=${longitude}`;
-
+  // Fetch API data
   let response = await fetch(currentUrl);
   console.log('Weatherbit API (current):', response.status, response.statusText, response.ok);
 
   if (response.ok) {
     let data = await response.json();
     return {
-      current_temperature: `${data.data[0].temp}℃`,
-      current_uv: data.data[0].uv.toFixed(1), // UV Index (0-11+)
-      current_humidity: `${data.data[0].rh}%`, // Relative humidity (%)
-      current_pressure: `${data.data[0].pres}mb`, // Pressure (mb)
-      current_wind_speed: `${data.data[0].wind_spd.toFixed(1)} m/s`,
-      current_wind_direction: getCardinalDirection(data.data[0].wind_dir),
-      current_icon: 'https://www.weatherbit.io/static/img/icons/' + data.data[0].weather.icon + '.png',
-      current_description: data.data[0].weather.description
+      date: data.data[0].ob_time.slice(0, 10),
+      temp: `${data.data[0].temp}℃`,
+      uv: data.data[0].uv.toFixed(1), // UV Index (0-11+)
+      humidity: `${data.data[0].rh}%`, // Relative humidity (%)
+      pressure: `${data.data[0].pres}mb`, // Pressure (mb)
+      wind_speed: `${data.data[0].wind_spd.toFixed(1)} m/s`,
+      wind_dir: getCardinalDirection(data.data[0].wind_dir),
+      icon: 'https://www.weatherbit.io/static/img/icons/' + data.data[0].weather.icon + '.png',
+      description: data.data[0].weather.description
     };
   } else {
     // Appropriately handle the error
     console.log(`Error: code ${response.status} ${response.statusText}!!!`);
     return {
-      current_temperature: 'no data',
-      current_uv: 'no data',
-      current_humidity: 'no data',
-      current_pressure: 'no data',
-      current_wind_speed: 'no data',
-      current_wind_direction: 'no data',
-      current_icon: 'no data',
-      current_description: 'no data'
+      date: 'no data',
+      temp: 'no data',
+      uv: 'no data',
+      humidity: 'no data',
+      pressure: 'no data',
+      wind_speed: 'no data',
+      wind_dir: 'no data',
+      icon: 'no data',
+      description: 'no data'
     };
   };
 };
